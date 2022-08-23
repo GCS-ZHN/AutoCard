eeb5d268d31c2c8e0e7f5af88b9bad206694e797
GCS-ZHN <zhang.h.n@foxmail.com>
2022-08-23 08:37:04 +0800

    use GPLv3 license

--- src/test/java/top/gcszhn/autocard/utils/HttpUtilsTest.java
+++ src/test/java/top/gcszhn/autocard/utils/HttpUtilsTest.java
@@ -1,17 +1,6 @@
-/*
- * Copyright © 2021 <a href="mailto:zhang.h.n@foxmail.com">Zhang.H.N</a>.
- *
- * Licensed under the Apache License, Version 2.0 (thie "License");
- * You may not use this file except in compliance with the license.
- * You may obtain a copy of the License at
- *
- *       http://wwww.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language govering permissions and
- * limitations under the License.
+/* 
+ * Copyright © 2022 <a href="mailto:zhang.h.n@foxmail.com">Zhang.H.N</a>.
+ * Release under GPL License
  */
 package top.gcszhn.autocard.utils;
 
